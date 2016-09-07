499fd7ff8642768c0b1585298b5c9281b959bd31
Tony Mai <tonymaibox@yahoo.com>
2016-09-07 14:18:52 -0400

    adding create a game function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,25 @@
 class GamesController < ApplicationController
+
+	def new
+		@game = Game.new
+		@random_game = Game.where('state = 0').first
+	end
+
+	def create
+		# binding.pry
+		@game = Game.create(state: 0)
+		@game.players << Player.create(user_id: User.create(name: "Jason").id, nickname: "jasonator")
+		redirect_to game_path(@game)
+	end
+
+	def show
+		binding.pry
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:user)
+	end
+
 end
