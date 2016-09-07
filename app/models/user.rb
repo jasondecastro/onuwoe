0fce86879022d20a58a35b4aea48c3669f26593b
kallaugher <alicekallaugher@gmail.com>
2016-09-07 14:16:21 -0400

    add sign in, sign out, sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
 	has_one :player
+
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :cards
   resources :games
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
