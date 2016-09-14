84979d072a07d022aac1ea5459c71601b8b86217
kallaugher <alicekallaugher@gmail.com>
2016-09-14 15:01:06 -0400

    refactor game controller #create method

--- app/services/game_generator.rb
+++ app/services/game_generator.rb
@@ -1,3 +1,10 @@
 class GameGenerator
-
+  def self.create_game(user)
+    game = Game.create(host: user.name, state: 0)
+    player = Player.create(user_id: user.id, nickname: user.name)
+    game.players << player
+    game.create_rounds
+    game.save
+    game
+  end
 end
