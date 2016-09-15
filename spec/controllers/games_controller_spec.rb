30e417aaecd4563540f4bf4f04dcd92cf18707ff
kallaugher <alicekallaugher@gmail.com>
2016-09-15 09:46:21 -0400

    add card generator to game controller spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -6,14 +6,7 @@ RSpec.describe GamesController, :type => :controller do
 
   before(:each) do
     allow(controller).to receive(:current_user).and_return(user)
-
-    Card.create(role: "Seer", action: "Seer, wake up. You may look at another player’s card or two of the center cards.", team: "Villager")
-    Card.create(role: "Villager", action: "", team: "Villager")
-    Card.create(role: "Villager", action: "", team: "Villager")
-    Card.create(role: "Troublemaker", action: "Troublemaker, wake up. You may exchange cards between two other players.", team: "Villager")
-    Card.create(role: "Robber", action: "Robber, wake up. You may exchange your card with another player’s card, and then view your new card.", team: "Villager")
-    Card.create(role: "Werewolf", action: "Find out the other werewolf.", team: "Werewolf")
-    Card.create(role: "Werewolf", action: "Find out the other werewolf.", team: "Werewolf")
+    CardGenerator.create_cards
   end
 
   describe "#create" do
